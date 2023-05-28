48d6237d489dda78ef2047400731be73293d87a1
longzewei <weilongzeplayer2@gmail.com>
2023-05-28 22:23:08 +0800

    control_flow

--- src/com/craftinginterpreters/lox/Stmt.java
+++ src/com/craftinginterpreters/lox/Stmt.java
@@ -7,8 +7,10 @@ abstract class Stmt {
   interface Visitor<R> {
     R visitBlockStmt(Block stmt);
     R visitExpressionStmt(Expression stmt);
+    R visitIfStmt(If stmt);
     R visitPrintStmt(Print stmt);
     R visitVarStmt(Var stmt);
+    R visitWhileStmt(While stmt);
   }
 
   // Nested Stmt classes here...
@@ -40,6 +42,24 @@ abstract class Stmt {
     final Expr expression;
   }
 //< stmt-expression
+//> stmt-if
+  static class If extends Stmt {
+    If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
+      this.condition = condition;
+      this.thenBranch = thenBranch;
+      this.elseBranch = elseBranch;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitIfStmt(this);
+    }
+
+    final Expr condition;
+    final Stmt thenBranch;
+    final Stmt elseBranch;
+  }
+//< stmt-if
 //> stmt-print
   static class Print extends Stmt {
     Print(Expr expression) {
@@ -70,6 +90,22 @@ abstract class Stmt {
     final Expr initializer;
   }
 //< stmt-var
+//> stmt-while
+  static class While extends Stmt {
+    While(Expr condition, Stmt body) {
+      this.condition = condition;
+      this.body = body;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitWhileStmt(this);
+    }
+
+    final Expr condition;
+    final Stmt body;
+  }
+//< stmt-while
 
   abstract <R> R accept(Visitor<R> visitor);
 }
